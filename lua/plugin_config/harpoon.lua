bee9533371af50c3e7069b670ebdfb589ce22e20
sylvia <sylviaviajung@gmail.com>
2022-12-30 23:09:39 -0800

    updated some stuff, mainly committing for port to laptop

--- lua/plugin_config/harpoon.lua
+++ lua/plugin_config/harpoon.lua
@@ -6,6 +6,15 @@ packwrap:AddPlugin("ThePrimeagen/harpoon", function()
   vim.keymap.set("n", "<leader>hl", require("harpoon.ui").nav_next)
   vim.keymap.set("n", "<leader>hh", require("harpoon.ui").nav_prev)
 
+  vim.keymap.set("n", "<leader>hm", require("harpoon.ui").toggle_quick_menu)
+
+  vim.keymap.set("n", "<leader>hm", require("harpoon.ui").toggle_quick_menu)
+
+  for i = 1, 9 do
+    vim.keymap.set("n", "<leader>h" .. i, function()
+      require("harpoon.ui").nav_file(i)
+    end)
+  end
+
   require("harpoon").setup()
-  require("telescope").load_extension("harpoon")
 end)
